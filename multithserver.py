7c4ecc788a607bb121207960127595723b9dfad9
sidstk <ssikev2@gmail.com>
2018-04-26 16:08:15 +0530

    Update multithserver.py
    main.py

--- multithserver.py
+++ multithserver.py
@@ -26,7 +26,8 @@ if sys.argv[2:]:
 
 server = ThreadingSimpleServer(('', port), SimpleHTTPRequestHandler)
 try:	
-	print("serving at port %s" %port)
+	print("\033[34mserving directory \033[1m{}\033[0m \033[34mat port \033[1m{}\033[0m".format(os.getcwd(),port))
+	print("\033[91m\033[1mpress ctrl-c to exit\033[0m")
 	httpd = threading.Thread(target=server.serve_forever())
 	httpd.daemon = True
 	httpd.start()
